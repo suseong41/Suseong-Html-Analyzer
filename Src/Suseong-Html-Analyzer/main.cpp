20a620c643b264953b8b50963807017a42886e66
박수성 <suseong0401@gmail.com>
2025-12-30 15:53:57 +0900

    init: 프로젝트 생성

---
+++ Src/Suseong-Html-Analyzer/main.cpp
@@ -0,0 +1,13 @@
+#include "pch.h"
+
+int main(int argc, char* argv[])
+{
+	if (1 < argc) 
+	{
+		printf("useage: %s [arguments]\n", argv[0]);
+	}
+
+	printf("Hello World!\n");
+
+	return 0;
+}
---
+++ Src/Suseong-Html-Analyzer/pch.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <stdio.h>
+
+// ExternalLib
+#include "../../../../cppcore/Inc/cppcore.h"
+using namespace core;
