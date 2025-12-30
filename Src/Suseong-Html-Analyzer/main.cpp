63eca728db5876ce1580fdab0e0e74a575a4fda5
박수성 <suseong0401@gmail.com>
2025-12-30 16:43:03 +0900

    feat: vs cppcore 링커 설정 및 시간 측정 추가

--- Src/Suseong-Html-Analyzer/main.cpp
+++ Src/Suseong-Html-Analyzer/main.cpp
@@ -1,21 +1,30 @@
 #include "pch.h"
+#include "HtmlParser.h"
 
 ECODE main(int argc, char* argv[])
 {
 	if (argc < 2 || 4 < argc)
 	{
-		printf("usage: ./Suseong-Html-Analyzer <htmlfile> [url]\n");
+		Log_Error("usage: ./Suseong-Html-Analyzer <htmlfile> [url]\n");
 		return EC_INVALID_ARGUMENT;
 	}
 
-	printf("Hello World!\n");
+	// 분석 시작
+	std::chrono::steady_clock::time_point startTime = std::chrono::steady_clock::now();
 
+	// 입력 인자 처리
 	char* htmlFile = argv[1];
 	char* targetUrl = NULL;
 	if (3 <= argc)
 	{
 		targetUrl = argv[2];
 	}
+
+	// 분석 종료
+	std::chrono::steady_clock::time_point endTime = std::chrono::steady_clock::now();
+	std::chrono::duration<double, std::milli> elapsed = endTime - startTime;
+
+	Log_Info("Html analysis completed in %.4f ms", elapsed.count());
 
 	return EC_SUCCESS;
 }
--- Src/Suseong-Html-Analyzer/pch.h
+++ Src/Suseong-Html-Analyzer/pch.h
@@ -1,7 +1,10 @@
 #pragma once
 
 #include <stdio.h>
+#include <chrono>
 
 // ExternalLib
 #include "../../../../cppcore/Inc/cppcore.h"
+
+#pragma comment (lib, "cppcore.lib")
 using namespace core;
