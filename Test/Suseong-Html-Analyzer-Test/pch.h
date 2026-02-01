3172e316ae89dd8f5b9cc867d8eb986df838c647
박수성 <suseong0401@gmail.com>
2026-02-01 17:55:57 +0900

    chore: 외부 라이브러리 가져오는 구조 변경에 맞춤

--- Test/Suseong-Html-Analyzer-Test/pch.h
+++ Test/Suseong-Html-Analyzer-Test/pch.h
@@ -1,8 +1,8 @@
 #pragma once
 
 #include <gtest/gtest.h>
-#pragma comment(lib, "gtest.lib")
-#pragma comment(lib, "gtest_main.lib")
+#pragma comment(lib, "gTest.lib")
+#pragma comment(lib, "gTest_main.lib")
 
 #include "../../../cppcore/Inc/cppcore.h"
 using namespace core;
