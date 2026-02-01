f02e14d9ba2b910029a44017ba66b3f463c93dc0
박수성 <suseong0401@gmail.com>
2026-02-01 20:38:42 +0900

    feat: 파일 로더로 전환

--- Test/Suseong-Html-Analyzer-Test/FileTest.cpp
+++ Test/Suseong-Html-Analyzer-Test/FileTest.cpp
@@ -1,45 +1,13 @@
 #include "pch.h"
-#include <fstream>
+#include "../../Src/Suseong-Html-Analyzer/HtmlLoader.h"
 
 // ASSERT -> 반환
 // EXPECT -> 계속 실행
 
 TEST(FileTest, ReadSample)
 {
-	const char* htmlFile = "../../Test/Sample/Sample.html";
-	FILE* fp = nullptr;
-
-#ifdef _WIN32
-	if (fopen_s(&fp, htmlFile, "rb") != 0 || fp == nullptr)
-	{
-		FAIL();
-		Log_Error("Failed open file: %s", htmlFile);
-		return;
-	}
-#else 
-	fp = fopen(htmlFile, "rb");
-	if (fp == nullptr)
-	{
-		FAIL();
-		return;
-	}
+	const char* path = "../../Test/Sample/Sample.html";
 	
-#endif
-
-	ASSERT_NE(fp, nullptr);
-
-	fseek(fp, 0, SEEK_END);
-	UINT64 fs = ftell(fp);
-	fseek(fp, 0, SEEK_SET);
-
-	EXPECT_GT(fs, 0); // empty file
-
-	std::vector<char> buffer(fs);
-	UINT64 readCount = fread(buffer.data(), 1, fs, fp);
-	EXPECT_EQ(readCount, fs); // file size check
-
-	fclose(fp);
-
-	std::string content(buffer.begin(), buffer.end());
-	EXPECT_NE(content.find("Suseong Analyzer"), std::string::npos); // check word
+	std::string html = HtmlLoad(path);
+	EXPECT_NE(html.find("Suseong Analyzer"), std::string::npos); // check word
 }
